f2b5f3b903e52567b90220cbe563695a330f3fa9
John Kosmicke <kozjob@yahoo.com>
2018-04-03 13:41:46 -0700

    update

---
+++ controller.js
@@ -0,0 +1,20 @@
+
+
+
+matcherController = {
+  model: matcherModel,
+  view: matcherView,
+
+  init: function( size ) {
+    this.model.init(size);
+    this.view.init();
+    this.view.addClickHandlers( this.selectCard, this );
+    this.view.updateGameView();    
+  },
+
+  selectCard: function( id ) {
+    
+  },
+    
+    
+};
